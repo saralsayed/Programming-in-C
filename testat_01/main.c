dbdddea97288c008f71626b253cd9d9c4c45c927
Sara El Souhagy <sarahlsayed@gmail.com>
2018-12-25 16:05:09 +0100

    Add files via upload

---
+++ testat_01/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int a1,a2;
+    printf("\n\n Hello TUHH! \n\n");
+    a1 = 50;
+    a2 = 22;
+    printf("%d + %d = %d \n",a1,a2,a1+a2);
+    printf("%d - %d = %d \n",a2,a1,a2-a1);
+    printf("%d * %d = %d \n",a1,a2,a1*a2);
+    printf("%d / %d = %f \n",a1,a2,(float)a1/a2);
+    return 0;
+}
---
+++ testat_01/testat_01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int a1,a2;
+    printf("\n\n Hello TUHH! \n\n");
+    a1 = 50;
+    a2 = 22;
+    printf("%d + %d = %d \n",a1,a2,a1+a2);
+    printf("%d - %d = %d \n",a2,a1,a2-a1);
+    printf("%d * %d = %d \n",a1,a2,a1*a2);
+    printf("%d / %d = %E \n",a1,a2,a1/a2);
+    return 0;
+}
+
