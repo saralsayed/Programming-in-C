621cf05bbf4af782ed119c2a0bfe1bbd68a4c007
Sara El Souhagy <sarahlsayed@gmail.com>
2018-12-25 16:13:27 +0100

    Create main.c

---
+++ testat_02/testat_02_01a/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x,y;
+    x = 50;
+    y = 10;
+    printf("\n %d + %d = %d \n",x,y,x+y);
+    printf("\n %d - %d = %d \n",x,y,x-y);
+    printf("\n %d * %d = %d \n",x,y,x*y);
+    return 0;
+}
