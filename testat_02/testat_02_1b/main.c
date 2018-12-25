1624de4d2f9d8dbe48a1f42c53114b97d8c53741
Sara El Souhagy <sarahlsayed@gmail.com>
2018-12-25 16:15:36 +0100

    Create main.c

---
+++ testat_02/testat_02_1b/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float x;
+    float y;
+    x = 7.5;
+    y = 4.3;
+    printf("\n %.2f + %.2f = %.2f \n",x,y,x+y);
+    printf("\n %.2f - %.2f = %.2f \n",x,y,x-y);
+    printf("\n %.2f * %.2f = %.2f \n",x,y,x*y);
+    return 0;
+}
